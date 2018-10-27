474214c9b2297caa79c6fb4a5fddd031f3439d44
robsnow68 <robsnow68@mac.com>
2018-10-27 11:20:42 +0100

    Split routes out into a separate controller

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,6 +7,11 @@ public func configure(
     _ env: inout Environment,
     _ services: inout Services
     ) throws { // 2
+    
+    var commandConfig = CommandConfig.default()
+    commandConfig.useFluentCommands()
+    services.register(commandConfig)
+    
     try services.register(FluentPostgreSQLProvider())
     let router = EngineRouter.default()
     try routes(router)
@@ -21,12 +26,31 @@ public func configure(
     let hostname = Environment.get("DATABASE_HOSTNAME")
         ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    //let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let databaseName: String
+    let databasePort: Int
+    // 1
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432
+    }
+    
+    
     let password = Environment.get("DATABASE_PASSWORD")
         ?? "password"
     // 3
+   // let databaseConfig = PostgreSQLDatabaseConfig(
+    //    hostname: hostname,
+    //    username: username,
+    //    database: databaseName,
+    //    password: password)
     let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
+        // 2
+        port: databasePort,
         username: username,
         database: databaseName,
         password: password)
@@ -40,7 +64,13 @@ public func configure(
     
     var migrations = MigrationConfig()
     // 4
+    migrations.add(model: User.self, database: .psql)
     migrations.add(model: Acronym.self, database: .psql)
+    migrations.add(model: Category.self, database: .psql)
+    migrations.add(
+        model: AcronymCategoryPivot.self,
+        database: .psql)
+    
     services.register(migrations)
 }
 
