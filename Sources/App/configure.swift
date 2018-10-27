a2a1b3b2ada015593b83e92469628e4e19548325
robsnow68 <robsnow68@mac.com>
2018-10-27 13:30:37 +0100

    remove tests

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -26,34 +26,19 @@ public func configure(
     let hostname = Environment.get("DATABASE_HOSTNAME")
         ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    //let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let databaseName: String
-    let databasePort: Int
-    // 1
-    if (env == .testing) {
-        databaseName = "vapor-test"
-        databasePort = 5433
-    } else {
-        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-        databasePort = 5432
-    }
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+   
     
     
     let password = Environment.get("DATABASE_PASSWORD")
         ?? "password"
     // 3
-   // let databaseConfig = PostgreSQLDatabaseConfig(
-    //    hostname: hostname,
-    //    username: username,
-    //    database: databaseName,
-    //    password: password)
-    let databaseConfig = PostgreSQLDatabaseConfig(
+   let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
-        // 2
-        port: databasePort,
         username: username,
         database: databaseName,
         password: password)
+
     // 4
     let database = PostgreSQLDatabase(config: databaseConfig)
     // 5
