ddb61b789dc020d2900e07efa3bf3ff28d291189
robsnow68 <robsnow68@mac.com>
2018-10-28 11:52:50 +0000

    Create and edit acronyms in web app

--- Sources/App/Models/Category.swift
+++ Sources/App/Models/Category.swift
@@ -17,10 +17,38 @@ extension Category: Migration {}
 extension Category: Parameter {}
 extension Category {
     // 1
-    var acronyms: Siblings<Category,
-    Acronym,
-    AcronymCategoryPivot> {
-    // 2
-    return siblings()
+    var acronyms: Siblings<Category,Acronym,AcronymCategoryPivot> {
+        return siblings()
+    }
+    
+        static func addCategory(
+            _ name: String,
+            to acronym: Acronym,
+            on req: Request
+            ) throws -> Future<Void> {
+            // 1
+            return Category.query(on: req)
+                .filter(\.name == name)
+                .first()
+                .flatMap(to: Void.self) { foundCategory in
+                    if let existingCategory = foundCategory {
+                        // 2
+                        return acronym.categories
+                            .attach(existingCategory, on: req)
+                            .transform(to: ())
+                    } else { // 3
+                        let category = Category(name: name)
+                        // 4
+                        return category.save(on: req)
+                            .flatMap(to: Void.self) { savedCategory in
+                                // 5
+                                return acronym.categories
+                                    .attach(savedCategory, on: req)
+                                    .transform(to: ())
+                        }
+                    } }
+        }
+ 
+   
 }
-}
+
